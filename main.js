fef3c0c7a87cd441520ed5cfad3be6f501c0bac6
jesse610 <jesse.velasquez40@gmail.com>
2023-03-20 12:53:17 -0700

    removed winner and play again btn after clicking

--- main.js
+++ main.js
@@ -55,33 +55,31 @@ const displayController = (() => {
         const divWinner = document.querySelector('.winner')
 
         if (gameController.player1.isWinner === true) {
-            div.textContent = "Player 1 wins!"
+            divWinner.textContent = "Player 1 wins!"
             playAgain()
         } else if (gameController.player2.isWinner === true) {
-            div.textContent = "Player 2 wins!"
+            divWinner.textContent = "Player 2 wins!"
             playAgain()
         } else if (gameController.player1.isWinner === false && gameController.player2.isWinner === false) {
-            div.textContent = "Tie!"
+            divWinner.textContent = "Tie!"
             playAgain()
-            // playAgain.showPlayAgainBtn()
-        }
-
-        return {
-            divWinner //<==== fix this 
         }
     }
 
     const playAgain = () => {
-        const div = document.createElement('div')
-        div.appendChild(btn)
+        const btnDiv = document.createElement('div')
+        const divWinner = document.querySelector('.winner')
+        btnDiv.appendChild(btn)
         btn.setAttribute('id', 'play-again-btn');
-        mainContainer.appendChild(div)
+        mainContainer.appendChild(btnDiv)
 
         btn.textContent = 'PLAY AGAIN'
 
         btn.addEventListener('click', function() {
-            restartGame.reset()
+            gameController.restartGame()
             updateBoard()
+            divWinner.remove()
+            btnDiv.remove()
         })
     }
 
@@ -122,10 +120,22 @@ const gameController = (() => {
             }
         })
     })
+
+    const restartGame = () => {
+        gameBoard.board = ['', '', '',
+                           '', '', '',
+                           '', '', ''];
+            player1.isWinner = undefined;
+            player1.turn = true;
+            player2.isWinner = undefined;
+            player2.turn = false;
+            counter.turnCount = 0
+    }
        
     return {
         player1,
         player2,
+        restartGame
     }
 })()
 
@@ -161,247 +171,4 @@ const winner = (() => {
         checkWin
     }
 })()
-
-const restartGame = (() => {
-
-    const reset = () => {
-        gameBoard.board = ['', '', '',
-                           '', '', '',
-                           '', '', ''];
-        gameController.player1.isWinner = undefined;
-        gameController.player1.turn = true;
-        gameController.player2.isWinner = undefined;
-        gameController.player2.turn = false;
-        counter.turnCount = 0
-    }
-    
-    return {reset}
-})()
-//     const updatePossibilties = (arr) => {
-//         arr.forEach((spaceEl, index) => {
-//             if(spaceEl != "") {
-//                 for (let i = 0; i < checkWinner.possibilities.length; i++) {
-//                     for (let j = 0; j < 3; j++)
-//                         if (index === checkWinner.possibilities[i][j]) {
-//                             checkWinner.possibilities[i][j] = spaceEl
-//                         }
-//                 }
-//             }
-//         })
-//       winner(checkWinner.possibilities)
-//     }
-
-//     const winner = (arr) => {
-//         for (let i = 0; i < possibilities.length; i++) { 
-//             if (arr[i].join('') === 'xxx') {
-//                 gameController.player2.isWinner = false
-//                 gameController.player1.isWinner = true
-//                 displayWinner.updateWinnerDisplay(gameController.player1)
-//             } else if (arr[i].join('') === 'ooo') {
-//                 gameController.player2.isWinner = true
-//                 gameController.player1.isWinner = false
-//                 displayWinner.updateWinnerDisplay(gameController.player2)
-//             } else if (gameController.turnCount === 10 && i === 7 && gameController.player1.isWinner === undefined && gameController.player2.isWinner === undefined) {
-//                 displayWinner.updateWinnerDisplay(gameController.player2)
-//             }
-//         }
-//     }
-//     return {
-//         winner,
-//         possibilities,
-//         updatePossibilties
-//     }
-// })()
-
-// const displayWinner = (() => {
-//     const mainContainer = document.querySelector('main')
-//     const gameboard = document.querySelector('.gameboard-container')
-//     const div = document.createElement('div')
-//     div.classList.add('winner')
-//     mainContainer.insertBefore(div, gameboard)
-
-//     const updateWinnerDisplay = (player) => {
-//         if (player.isWinner === true && player.letter === 'x') {
-//             div.textContent = "Player 1 wins!"
-//             playAgain.showPlayAgainBtn()
-//             playAgain.replayGame()
-//         } else if (player.isWinner === true && player.letter === 'o') {
-//             div.textContent = "Player 2 wins!"
-//             playAgain.showPlayAgainBtn()
-//         } else if (gameController.turnCount === 10 && player.isWinner === undefined) {
-//             div.textContent = "Tie!"
-//             playAgain.showPlayAgainBtn()
-//         }
-//     }
-
-//     return {
-//         updateWinnerDisplay
-//     }
-
-// })()
-
-// const playAgain = (() => {
-
-//     const mainContainer = document.querySelector('main')
-//     const div = document.createElement('div')
-//     const btn = document.createElement('button')
-//     btn.textContent = "PLAY AGAIN"
-//     btn.setAttribute('id', 'play-again-btn')
-//     div.appendChild(btn)
-
-//     const showPlayAgainBtn = () => {
-//         mainContainer.appendChild(div)
-//     }
-
-//     const replayGame = () => {
-//         const playAgainBtn = document.querySelector('#play-again-btn')
-//         playAgainBtn.addEventListener('click', function() {
-//             Gameboard.board = ['', '', '',
-//                                '', '', '',
-//                                '', '', ''];
-//             Gameboard.boardPieces.forEach((el, index) => {
-//                 Gameboard.boardPieces[index].textContent = '' 
-//             })
-
-//             gameController.turnCount = 1
-//             gameController.player2.isWinner = undefined
-//             gameController.player1.isWinner = undefined
-//             checkWinner.possibilities = [
-//                 [0, 1, 2],
-//                 [0, 3, 6],
-//                 [0, 4, 8],
-//                 [1, 4, 7],
-//                 [2, 5, 8],
-//                 [2, 4, 6],
-//                 [3, 4, 5],
-//                 [6, 7, 8],
-//             ]
-//             console.log(Gameboard.boardPieces)
-//             console.log(Gameboard.board)
-//         })
-//     }
-
-//     return {
-//         showPlayAgainBtn,
-//         replayGame
-//     }
-// })()
-//         [0, 3, 6],
-//         [0, 4, 8],
-//         [1, 4, 7],
-//         [2, 5, 8],
-//         [2, 4, 6],
-//         [3, 4, 5],
-//         [6, 7, 8],
-//     ]
-
-//     const updatePossibilties = (arr) => {
-//         arr.forEach((spaceEl, index) => {
-//             if(spaceEl != "") {
-//                 for (let i = 0; i < checkWinner.possibilities.length; i++) {
-//                     for (let j = 0; j < 3; j++)
-//                         if (index === checkWinner.possibilities[i][j]) {
-//                             checkWinner.possibilities[i][j] = spaceEl
-//                         }
-//                 }
-//             }
-//         })
-//       winner(checkWinner.possibilities)
-//     }
-
-//     const winner = (arr) => {
-//         for (let i = 0; i < possibilities.length; i++) { 
-//             if (arr[i].join('') === 'xxx') {
-//                 gameController.player2.isWinner = false
-//                 gameController.player1.isWinner = true
-//                 displayWinner.updateWinnerDisplay(gameController.player1)
-//             } else if (arr[i].join('') === 'ooo') {
-//                 gameController.player2.isWinner = true
-//                 gameController.player1.isWinner = false
-//                 displayWinner.updateWinnerDisplay(gameController.player2)
-//             } else if (gameController.turnCount === 10 && i === 7 && gameController.player1.isWinner === undefined && gameController.player2.isWinner === undefined) {
-//                 displayWinner.updateWinnerDisplay(gameController.player2)
-//             }
-//         }
-//     }
-//     return {
-//         winner,
-//         possibilities,
-//         updatePossibilties
-//     }
-// })()
-
-// const displayWinner = (() => {
-//     const mainContainer = document.querySelector('main')
-//     const gameboard = document.querySelector('.gameboard-container')
-//     const div = document.createElement('div')
-//     div.classList.add('winner')
-//     mainContainer.insertBefore(div, gameboard)
-
-//     const updateWinnerDisplay = (player) => {
-//         if (player.isWinner === true && player.letter === 'x') {
-//             div.textContent = "Player 1 wins!"
-//             playAgain.showPlayAgainBtn()
-//             playAgain.replayGame()
-//         } else if (player.isWinner === true && player.letter === 'o') {
-//             div.textContent = "Player 2 wins!"
-//             playAgain.showPlayAgainBtn()
-//         } else if (gameController.turnCount === 10 && player.isWinner === undefined) {
-//             div.textContent = "Tie!"
-//             playAgain.showPlayAgainBtn()
-//         }
-//     }
-
-//     return {
-//         updateWinnerDisplay
-//     }
-
-// })()
-
-// const playAgain = (() => {
-
-//     const mainContainer = document.querySelector('main')
-//     const div = document.createElement('div')
-//     const btn = document.createElement('button')
-//     btn.textContent = "PLAY AGAIN"
-//     btn.setAttribute('id', 'play-again-btn')
-//     div.appendChild(btn)
-
-//     const showPlayAgainBtn = () => {
-//         mainContainer.appendChild(div)
-//     }
-
-//     const replayGame = () => {
-//         const playAgainBtn = document.querySelector('#play-again-btn')
-//         playAgainBtn.addEventListener('click', function() {
-//             Gameboard.board = ['', '', '',
-//                                '', '', '',
-//                                '', '', ''];
-//             Gameboard.boardPieces.forEach((el, index) => {
-//                 Gameboard.boardPieces[index].textContent = '' 
-//             })
-
-//             gameController.turnCount = 1
-//             gameController.player2.isWinner = undefined
-//             gameController.player1.isWinner = undefined
-//             checkWinner.possibilities = [
-//                 [0, 1, 2],
-//                 [0, 3, 6],
-//                 [0, 4, 8],
-//                 [1, 4, 7],
-//                 [2, 5, 8],
-//                 [2, 4, 6],
-//                 [3, 4, 5],
-//                 [6, 7, 8],
-//             ]
-//             console.log(Gameboard.boardPieces)
-//             console.log(Gameboard.board)
-//         })
-//     }
-
-//     return {
-//         showPlayAgainBtn,
-//         replayGame
-//     }
-// })()
 
